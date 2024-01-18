796b558f3dfe3cbef36cb88b6199cc32e702068b
ernkeyz <exconzy26@gmail.com>
2024-01-18 11:48:59 +0000

    JavaScript Refresher-Arrows Functions

---
+++ React_The_Complete_Guide/02_JavaScript_Refresher/arrow_functions.js
@@ -0,0 +1,6 @@
+// Arrows Functions - Modern way of creating JavaScript functions
+// solves a lot of issues with the 'this' keyword in JavaScript which sometimes doesn't refer to what you're refering to.
+
+const myName = (name) => name;
+
+console.log(myName("Ernest"));
