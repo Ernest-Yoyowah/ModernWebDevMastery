3591213ebb3c4b47ddc57b9874a33bab2b42c41d
ernkeyz <exconzy26@gmail.com>
2024-01-26 16:58:53 +0000

    Extracting Dynamic Parameter Values

--- React_The_Complete_Guide/NextJS/codebase_1/pages/news/[newsId].js
+++ React_The_Complete_Guide/NextJS/codebase_1/pages/news/[newsId].js
@@ -1,6 +1,11 @@
 import React from "react";
+import { useRouter } from "next/router";
 
 const NewsID = () => {
+  const router = useRouter();
+
+  router.query.newsId;
+
   return <h1>The NewsID Page</h1>;
 };
 
