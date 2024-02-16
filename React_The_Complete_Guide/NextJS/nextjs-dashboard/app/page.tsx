ee7b0cd5bcb55104e1978dd1ba38b55694b1c996
ernkeyz <exconzy26@gmail.com>
2024-02-16 00:49:26 +0000

    adding Images to page

--- React_The_Complete_Guide/NextJS/nextjs-dashboard/app/page.tsx
+++ React_The_Complete_Guide/NextJS/nextjs-dashboard/app/page.tsx
@@ -5,6 +5,8 @@ import Link from 'next/link';
 import styles from '@/app/ui/home.module.css';
 import { lusitana } from './ui/fonts';
 
+import Image from 'next/image';
+
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
@@ -32,6 +34,20 @@ export default function Page() {
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
+          <Image
+            src="/hero-desktop.png"
+            width={1000}
+            height={760}
+            className="hidden md:block"
+            alt="Screenshots of the dashboard project showing desktop version"
+          />
+          <Image
+            src="/hero-mobile.png"
+            width={560}
+            height={620}
+            className="block md:hidden"
+            alt="Screenshot of the dashboard project showing mobile version"
+          />
         </div>
       </div>
     </main>
