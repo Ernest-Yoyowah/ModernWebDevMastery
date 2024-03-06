37331fe7560c8ec2e610d784faa299484e9c034f
ernkeyz <exconzy26@gmail.com>
2024-03-06 12:27:02 +0000

    list of movies

--- React_The_Complete_Guide/practice_codebase/src/components/MovieList.js
+++ React_The_Complete_Guide/practice_codebase/src/components/MovieList.js
@@ -3,17 +3,17 @@ import { useState } from "react";
 const MovieList = () => {
   const [movies, setMovies] = useState([
     {
-      name: "Ernest",
+      name: "The Social Network",
       price: "$30.32",
       id: 4231,
     },
     {
-      name: "Nii",
+      name: "The Man",
       price: "$32",
       id: 4232,
     },
     {
-      name: "Yoyowah",
+      name: "Law of Life",
       price: "$42",
       id: 4233,
     },
