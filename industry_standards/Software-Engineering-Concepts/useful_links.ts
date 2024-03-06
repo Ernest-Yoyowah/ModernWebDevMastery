26e545f771deaa4aad3803cf82aa53d2c1b62748
ernkeyz <exconzy26@gmail.com>
2024-03-06 20:41:07 +0000

    saving some useful links and resources here

---
+++ industry_standards/Software-Engineering-Concepts/useful_links.ts
@@ -0,0 +1,13 @@
+// some useful links
+
+export const usefulLinks = {
+  curlCommand: "curl -I -X GET http://harvard.edu/",
+  curlCommandSecure: "curl -I -X GET https://www.harvard.edu/",
+  tcpipGuide: "https://www.tcpipguide.com/",
+  stackOverflow: "https://stackoverflow.com/",
+  mdnWebDocs: "https://developer.mozilla.org/",
+  w3Schools: "https://www.w3schools.com/",
+  devCommunity: "https://dev.to/",
+  github: "https://github.com/",
+  "add bable to a project": `<script src="https://unpkg.com/@babel/standalone/babel.min.js"></script>`,
+};
