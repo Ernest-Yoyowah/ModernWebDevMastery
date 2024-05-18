5909212ae4445207991dd910c417bb954af5f26d
Ernest Yoyowah <ernestniiyoyowah@gmail.com>
2024-05-18 18:50:44 +0000

    Added resolver functions for queries

--- data_query_languages/GraphQL/ApolloServer/index.js
+++ data_query_languages/GraphQL/ApolloServer/index.js
@@ -1,7 +1,7 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
-import { typeDefs } from "./schema";
-import db from "./_db";
+import { typeDefs } from "./schema.js";
+import db from "./_db.js";
 
 const resolvers = {
   Query: {
