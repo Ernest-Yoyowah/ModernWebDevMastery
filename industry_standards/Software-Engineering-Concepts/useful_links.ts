9d14dd8bc5425dd12e699828fe6cb041070d1f06
Ernest Yoyowah <ernestniiyoyowah@gmail.com>
2024-07-30 10:22:11 +0000

    update

--- industry_standards/Software-Engineering-Concepts/useful_links.ts
+++ industry_standards/Software-Engineering-Concepts/useful_links.ts
@@ -1,6 +1,6 @@
 // some useful links
 
-export const usefulLinks: {} = {
+export const usefulLinks: { [key: string]: string } = {
   curlCommand: "curl -I -X GET http://harvard.edu/",
   curlCommandSecure: "curl -I -X GET https://www.harvard.edu/",
   tcpipGuide: "https://www.tcpipguide.com/",
