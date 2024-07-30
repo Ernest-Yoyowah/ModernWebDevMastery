d75d74503b1444f184799e86d05a2b0568bb1e65
Ernest Yoyowah <ernestniiyoyowah@gmail.com>
2024-07-30 10:18:25 +0000

    update

--- industry_standards/Software-Engineering-Concepts/useful_links.ts
+++ industry_standards/Software-Engineering-Concepts/useful_links.ts
@@ -1,6 +1,6 @@
 // some useful links
 
-export const usefulLinks = {
+export const usefulLinks: any = {
   curlCommand: "curl -I -X GET http://harvard.edu/",
   curlCommandSecure: "curl -I -X GET https://www.harvard.edu/",
   tcpipGuide: "https://www.tcpipguide.com/",
@@ -12,12 +12,13 @@ export const usefulLinks = {
   MIT: "https://web.mit.edu/6.031/www/fa21/",
   bable: `<script src="https://unpkg.com/@babel/standalone/babel.min.js"></script>`, // add bable to a project
   package_installatiion: "npm install --legacy-peer-deps", // installing order packages in proj if any
-  evince: "open pdf in terminal"
-  disable-web-security: "google-chrome --disable-web-security --user-data-dir=~/focus-technologies/chrome_data_dir"
-  hex-color: "https://www.color-hex.com"
-  Animations: "https://lottiefiles.com/free-animations/front-end-developer"
-  google.com: "172.217.16.78"
-  youtube.com: "142.250.181.206"
-  facebook.com: "157.240.210.35"
-  amazon.com: "205.251.242.103"
+  evince: "open pdf in terminal",
+  "disable-web-security":
+    "google-chrome --disable-web-security --user-data-dir=~/focus-technologies/chrome_data_dir",
+  "hex-color": "https://www.color-hex.com",
+  Animations: "https://lottiefiles.com/free-animations/front-end-developer",
+  "google.com": "172.217.16.78",
+  "youtube.com": "142.250.181.206",
+  "facebook.com": "157.240.210.35",
+  "amazon.com": "205.251.242.103",
 };
